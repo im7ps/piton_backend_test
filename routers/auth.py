7d152aae44484a0d7792a9e3e4df0a86380f5447
Stefano Gerace <sgerace@e4r13p2.42roma.it>
2023-09-27 20:11:00 +0200

    test su auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -22,7 +22,16 @@ ALGORITHM = 'HS256'
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
-
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 
@@ -32,20 +41,14 @@ db_dependency = Annotated[Session, Depends(get_db)]
 def authenticate_user(username: str, password: str, db):
     user = db.query(Users).filter(Users.username == username).first()
     if not user:
-        print("AUTHENTICATE USER NOT WORKING\n")
         return False
     if not bcrypt_context.verify(password, user.hashed_password):
-        print("AUTHENTICATE USER NOT WORKING 2\n")
         return False
     return user
-
-
 def create_access_token(username: str, user_id: int, role: str, expires_delta: timedelta):
     expires = datetime.utcnow() + expires_delta
     encode = {'sub': username, 'id': user_id, 'role': role, 'exp': expires}
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
-
-
 async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -59,15 +62,14 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail='Could not validate user.')
-
-
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+async def create_user(db: db_dependency,
+                      create_user_request: CreateUserRequest):
+    #da inserire verifica email non ancora in uso
     try:
         verify_email(db, create_user_request.email)
     except:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Email not valid.')
-
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -77,19 +79,14 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
         hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
-
     db.add(create_user_model)
     db.commit()
-
-
 @router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                  db: db_dependency):
-    print("asdasdasd\n")
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail='Could not validate user.')
     token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
-
     return {'access_token': token, 'token_type': 'bearer'}
